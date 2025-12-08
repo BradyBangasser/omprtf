48bdbdf5a92ebe09f44c913a3d0fdeb0a7cf1683
Brady Bangasser <bradybbangasser@gmail.com>
2025-12-08 16:04:31 -0600

    Fixed pass

--- eval/basic/main.c
+++ eval/basic/main.c
@@ -7,7 +7,7 @@ int test() {
 
 int main() {
   int x[200000] = {};
-  // #pragma omp target map(tofrom : x)
+#pragma omp target map(tofrom : x)
   for (int i = 0; i < sizeof(x) / sizeof(*x); i++) {
     x[i] = i;
   }
