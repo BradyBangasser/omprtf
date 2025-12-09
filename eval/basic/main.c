8d747a66a8d5962053cee266c36e405191b0c6af
Brady Bangasser <bradybbangasser@gmail.com>
2025-12-09 09:26:45 -0600

    Adding metadata pass

--- eval/basic/main.c
+++ eval/basic/main.c
@@ -4,7 +4,7 @@
 
 int main() {
   int *x = malloc(N * sizeof(int));
-#pragma omp target map(tofrom : x)
+#pragma omp target map(tofrom : x[0 : N])
   for (int i = 0; i < N; i++) {
     x[i] = i;
   }
