3c3aa0cab38d5abf61c4fc20154a1a6f6d9fc7b1
Brady Bangasser <bradybbangasser@gmail.com>
2025-12-13 11:56:46 -0600

    Testing single exe

---
+++ src/omprtf/main.cpp
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <clang/Driver/Driver.h>
+#include <llvm/Support/Duration.h>
+#include <llvm/Support/ErrorOr.h>
+#include <llvm/Support/Program.h>
+#include <llvm/TargetParser/Triple.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "cmeta.h"
+#include "logging.h"
+#include "preload.hh"
+
+int main(int argc, const char *argv[]) {
+  assert(argc == 2);
+
+  // Augment metadata
+  cmeta(argv[1]);
+
+  llvm::ErrorOr<std::string> clang_p = llvm::sys::findProgramByName("clang");
+
+  if (!clang_p) {
+    ERROR("Failed to locate Clang\n");
+    return 1;
+  }
+
+  char const *c_arg_arr[] = {clang_p->data(),
+                             "-g",
+                             "-lomp",
+                             "-fopenmp",
+                             "-fopenmp-targets=nvptx64-nvidia-cuda",
+                             "-o",
+                             "/tmp/out.out",
+                             argv[1],
+                             NULL};
+
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    DEBUGF("%s\n", clang_p->data());
+    execv(clang_p->data(), (char **)c_arg_arr) || (SUCCESS("Worked\n"));
+    ERROR("ERROR\n");
+    exit(127);
+  } else {
+    waitpid(pid, 0, 0);
+  }
+
+  std::vector<char *> pargs;
+  char *program = (char *)argv[1];
+  pargs.push_back(program);
+  for (int i = 1; i < argc; ++i) {
+    pargs.push_back((char *)argv[i]);
+  }
+  pargs.push_back(nullptr);
+
+  setenv_omp_tool();
+  setenv_omp_tool_libraries(argv[0]);
+  setenv_omp_tool_verbose_init(0);
+
+  execvp(program, pargs.data());
+  return 0;
+}
